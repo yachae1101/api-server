ef50f3ca47d78e754510124a79346d12aa01ed85
yachae1101 <yachae1101@naver.com>
2024-11-28 22:50:54 +0900

    CORS url changed

--- src/main/java/com/yachae1101/apiserver/config/WebConfig.java
+++ src/main/java/com/yachae1101/apiserver/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://byeong630-front-app.s3-website.ap-northeast-2.amazonaws.com");
     }
 }
