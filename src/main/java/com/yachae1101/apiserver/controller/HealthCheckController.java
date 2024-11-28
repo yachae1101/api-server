7dfb91613cbffca4e844ddcbba1aca3fe5f46190
yachae1101 <yachae1101@naver.com>
2024-11-28 21:39:07 +0900

    health check controller added

---
+++ src/main/java/com/yachae1101/apiserver/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.yachae1101.apiserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "Application is running!";
+    }
+}
